0d253deccd39194a58a35e439c384be5240d8e91
Chrispin Kabuya <chrispin.kabuya@gmail.com>
2022-01-07 14:52:27 +0200

    start stage 5 complete

--- Code Sharing Platform/task/src/platform/controller/ApiCodeController.java
+++ Code Sharing Platform/task/src/platform/controller/ApiCodeController.java
@@ -1,8 +1,10 @@
 package platform.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
 import platform.model.Code;
 import platform.service.CodeService;
@@ -13,6 +15,7 @@ import java.util.stream.Collectors;
 
 @RestController
 public class ApiCodeController {
+
     @Autowired
     CodeService dbService;
 
@@ -28,8 +31,16 @@ public class ApiCodeController {
      * GET /api/code/N should return JSON with the N-th uploaded code snippet.
      */
     @GetMapping(path = "/api/code/{id}")
-    public Code getCodeAsJSON(@PathVariable Long id) {
-        return  dbService.getCode(id);
+    public Code getCodeAsJSON(@PathVariable String id) {
+        System.out.println("Processing Get API:" +id);
+        Code code = dbService.getCode(id);
+        System.out.println("Code Processed via API:" +code);
+        if(code !=null){
+            return code;
+        }
+        else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND," Not found");
+        }
     }
 
 
@@ -40,7 +51,12 @@ public class ApiCodeController {
      * */
     @PostMapping(path = "/api/code/new")
     public String postCode(@RequestBody Code newCode) {
-        return "{\"id\":\""+ dbService.addCode(newCode)+"\"}";
+        if(newCode.getTime()>0 || newCode.getViews() > 0 ){
+            newCode.setSecret(true);
+        }
+        String uid=dbService.addCode(newCode);
+        System.out.println("ID: "+uid+ " Code :" +newCode);
+        return "{\"id\":\""+uid +"\"}";
     }
     /*
     GET /api/code/latest should return a JSON array with 10 most recently uploaded code snippets sorted from the newest to the oldest.
